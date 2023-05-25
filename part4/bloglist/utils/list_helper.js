2b2fae40e9cde900526bb7d5dcdcbf19e8bd9b14
jaggerharness <jaggerharness5@gmail.com>
2023-05-24 21:53:24 -0500

    unit tests - bloglist

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,9 +4,23 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
+  return blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    0
+  );
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return blogs;
+  }
+  return blogs.reduce((accumulator, currentValue) =>
+    accumulator.likes > currentValue.likes ? accumulator : currentValue
+  );
 };
 
 module.exports = {
-  dummy, totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog,
 };
