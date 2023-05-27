28174f5339f2d6daad5ec9e40a3975d155e7d0ba
jaggerharness <jaggerharness5@gmail.com>
2023-05-26 20:51:27 -0500

    deps - unit tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require("lodash");
+
 const dummy = (blogs) => {
   console.log(blogs);
   return 1;
@@ -19,8 +21,46 @@ const favoriteBlog = (blogs) => {
   );
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return blogs;
+  }
+
+  const authorCounts = _.countBy(blogs, "author");
+  const authorWithMostBlogs = _.maxBy(
+    _.keys(authorCounts),
+    (author) => authorCounts[author]
+  );
+
+  const numberOfBlogs = authorCounts[authorWithMostBlogs];
+
+  return {
+    author: authorWithMostBlogs,
+    blogs: numberOfBlogs,
+  };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return blogs;
+  }
+
+  const likesByAuthor = _.groupBy(blogs, 'author');
+  const authorLikes = _.mapValues(likesByAuthor, (blogs) => _.sumBy(blogs, 'likes'));
+  const authorWithMostLikes = _.maxBy(_.keys(authorLikes), (author) => authorLikes[author]);
+
+  const numberOfLikes = authorLikes[authorWithMostLikes];
+
+  return {
+    author: authorWithMostLikes,
+    likes: numberOfLikes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
