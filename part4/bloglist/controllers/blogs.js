41cc176266616044b24df9212e565c17e431e156
jaggerharness <jaggerharness5@gmail.com>
2023-06-01 21:07:09 -0500

    Exercise 4.13

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -9,7 +9,7 @@ blogRouter.get("/", async (req, res) => {
 blogRouter.post("/", async (req, res) => {
   const body = req.body;
 
-  if(!body.title || !body.url){
+  if (!body.title || !body.url) {
     res.status(400).end();
     return;
   }
@@ -19,10 +19,22 @@ blogRouter.post("/", async (req, res) => {
     author: body.author,
     url: body.url,
     likes: body.likes || false,
-  })
+  });
 
   const savedBlog = await blog.save();
-  res.status(201).json(savedBlog)
+  res.status(201).json(savedBlog);
+});
+
+blogRouter.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(400).end();
+    return;
+  }
+
+  await Blog.findByIdAndDelete(id);
+  res.status(204).end();
 });
 
 module.exports = blogRouter;
