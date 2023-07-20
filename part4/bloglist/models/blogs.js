7cfd8d9f9ea3354136cddc5dfae2b00fda849280
jaggerharness <jagger.dev@gmail.com>
2023-07-20 13:28:04 -0500

    update blogs model

--- part4/bloglist/models/blogs.js
+++ part4/bloglist/models/blogs.js
@@ -14,6 +14,7 @@ blogSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    // delete returnedObject.__v;
   },
 });
 
