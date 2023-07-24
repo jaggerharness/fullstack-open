b60b8193e9b38a8a00920378eb9fbaa5f5c52589
jaggerharness <jagger.dev@gmail.com>
2023-07-24 09:43:03 -0500

    Exercise 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blogs");
+const User = require("../models/users");
 
 blogRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   res.json(blogs);
 });
 
@@ -14,14 +19,20 @@ blogRouter.post("/", async (req, res) => {
     return;
   }
 
+  const user = await User.findOne({});
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || false,
+    user: user.id,
   });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   res.status(201).json(savedBlog);
 });
 
