72e539ecb108d7744aa603f06bcfadf8421920c6
jaggerharness <jagger.dev@gmail.com>
2023-08-05 22:32:56 -0500

    Exercise 5.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 require('express-async-errors');
 const app = express();
-const cors = require("cors");
-const blogRouter = require("./controllers/blogs");
-const userRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login");
-const middleware = require("./utils/middleware");
+const cors = require('cors');
+const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+const middleware = require('./utils/middleware');
 
 const requestLogger = middleware.requestLogger;
 const tokenExtractor = middleware.tokenExtractor;
@@ -17,9 +17,15 @@ app.use(requestLogger);
 app.use(tokenExtractor);
 app.use(userExtractor);
 
-app.use("/api/blogs", userExtractor, blogRouter);
-app.use("/api/users", userRouter);
-app.use("/api/login", loginRouter);
+app.use('/api/blogs', userExtractor, blogRouter);
+app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
+
+// Only available when running tests
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,9 @@
 describe('Blog application', () => {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    cy.visit('http://localhost:5173');
+  });
   it('front end can be accessed', () => {
-    cy.visit('https://localhost:5173');
     cy.contains('Blogs');
   });
 });
