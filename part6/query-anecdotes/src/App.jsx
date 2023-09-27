f63e91a1fcd87675e458988ab187b5984d642b88
jaggerharness <jagger.dev@gmail.com>
2023-09-27 11:15:44 -0500

    feat: fetch anecdotes from json server

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,40 +1,64 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
 
-const App = () => {
-
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
-
+const AnecdoteList = ({ anecdotes, handleVote }) => {
   return (
     <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
+      ))}
+    </div>
+  );
+};
+
+const LoadingIndicator = () => {
+  return <div>Loading data...</div>;
+};
+
+const ErrorIndicator = () => {
+  return (
+    <div>
+      Unexpected error occurred while fetching anecdotes. Please, try again.
+    </div>
+  );
+};
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log(anecdote);
+  };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: () =>
+      axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+  });
+
+  const anecdotes = data;
+
+  return (
+    <div>
+      {isLoading && <LoadingIndicator />}
+      {isError && <ErrorIndicator />}
+      {data && (
+        <>
+          <h3>Anecdote app</h3>
+          <Notification />
+          <AnecdoteForm />
+          <AnecdoteList anecdotes={data} handleVote={handleVote} />
+        </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
